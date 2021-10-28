863acd6a2b4cc671287df8f9724267770b2b2142
Mudit Kapoor <mukapoor@paypal.com>
2021-10-29 01:00:16 +0530

    Initial packer

--- init.vim
+++ init.vim
@@ -2,26 +2,7 @@
 
 " Plugins
 
-call plug#begin(stdpath('data') . '/plugged')
-
-    Plug stdpath('data') . '/swift-vim'
-    Plug 'tpope/vim-commentary'
-    Plug 'tpope/vim-fugitive'
-    Plug 'airblade/vim-gitgutter'
-    " Plug 'dracula/vim', { 'as': 'dracula' }
-    " Plug 'morhetz/gruvbox'
-    Plug 'sainnhe/gruvbox-material'
-    " Plug 'machakann/vim-sandwich'
-    Plug 'tpope/vim-surround'
-    Plug 'tpope/vim-repeat'
-    Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
-    Plug 'junegunn/fzf.vim'
-    Plug 'vim-airline/vim-airline'
-    Plug 'wellle/targets.vim'
-    Plug 'justinmk/vim-sneak'
-
-call plug#end()
-
+lua require('plugins')
 " let g:gruvbox_contrast_dark='soft'
 colorscheme gruvbox-material
 
---
+++ lua/plugins.lua
@@ -0,0 +1,45 @@
+local fn = vim.fn
+
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  })
+end
+
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself https://github.com/wbthomason/packer.nvim
+    use 'wbthomason/packer.nvim'
+    use {
+      fn.stdpath('data') .. '/swift-vim'
+    }
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = {'kyazdani42/nvim-web-devicons', opt = true}
+    }
+    use 'kyazdani42/nvim-web-devicons'
+
+    use 'sainnhe/gruvbox-material'
+
+    if packer_bootstrap then
+      require('packer').sync()
+    end
+end)
+-- call plug#begin(stdpath('data') . '/plugged')
+
+--     Plug 'tpope/vim-commentary'
+--     Plug 'tpope/vim-fugitive'
+--     Plug 'airblade/vim-gitgutter'
+--     Plug 'tpope/vim-surround'
+--     Plug 'tpope/vim-repeat'
+--     Plug 'vim-airline/vim-airline'
+--     Plug 'wellle/targets.vim'
+--     Plug 'justinmk/vim-sneak'
+
+-- call plug#end()
