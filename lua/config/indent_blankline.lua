64052c344dfa5d04954e77d1623283adfee6b224
Mudit Kapoor <mukapoor@paypal.com>
2021-12-07 18:46:46 +0530

    Add filetype excludes

--- lua/config/indent_blankline.lua
+++ lua/config/indent_blankline.lua
@@ -1,11 +1,17 @@
 require("indent_blankline").setup {
-  char = "▏",
+  -- char = "▏",
   show_trailing_blankline_indent = false,
   show_first_indent_level = true,
   show_current_context = true,
-  char_list = {"|", "¦", "┆", "┊"},
+  -- char_list = {"|", "¦", "┆", "┊"},
   context_patterns = {
     "class", "function", "method", "block", "list_literal", "selector",
     "^if", "^table", "if_statement", "while", "for"
-  }
+  },
+  filetype_exclude = {
+    "log", "fugitive", "gitcommit",
+    "packer", "vimwiki", "txt", "help",
+    "git", "TelescopePrompt", "undotree",
+    -- "" -- for all buffers without a file type
+  },
 }
