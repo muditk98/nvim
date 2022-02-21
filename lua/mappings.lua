3bcc208cac5fd8324be3b1e02248f86e30f776cf
Mudit Kapoor <mukapoor@paypal.com>
2022-02-21 16:01:05 +0530

    Use lua map for omni s

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,33 +1,31 @@
 local map = vim.api.nvim_set_keymap
 
 local opts = {noremap = true}
--- vim.g.mapleader = "\\"
--- map("", "<Space>", "<Leader>", {noremap = true})
-vim.cmd("map <Space> <Leader>")
+-- vim.g.mapleader = '\\'
+-- map('', '<Space>', '<Leader>', {noremap = true})
+vim.cmd('map <Space> <Leader>')
 
--- map("v", "<", "<gv", {noremap = true})
--- map("v", ">", ">gv", {noremap = true})
+-- map('v', '<', '<gv', {noremap = true})
+-- map('v', '>', '>gv', {noremap = true})
 
 -- Tab switch buffer
--- map("n", "<TAB>", ":bnext<CR>", {noremap = true})
--- map("n", "<S-TAB>", ":bprevious<CR>", {noremap = true})
+-- map('n', '<TAB>', ':bnext<CR>', {noremap = true})
+-- map('n', '<S-TAB>', ':bprevious<CR>', {noremap = true})
 
 -- Find files using Telescope command-line sugar.
-map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
-map("n", "<leader>fl", "<cmd>Telescope live_grep<cr>", opts)
-map("n", "<leader>fg", "<cmd>Telescope grep_string<cr>", opts)
-map("n", "<leader>fb", "<cmd>Telescope buffers<cr>", opts)
-map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", opts)
-map("n", "z=", "<cmd>Telescope spell_suggest<cr>", opts)
-vim.cmd [[
-  nmap s <Plug>Lightspeed_omni_s
-]]
+map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts)
+map('n', '<leader>fl', '<cmd>Telescope live_grep<cr>', opts)
+map('n', '<leader>fg', '<cmd>Telescope grep_string<cr>', opts)
+map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', opts)
+map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', opts)
+map('n', 'z=', '<cmd>Telescope spell_suggest<cr>', opts)
+map('n', 's', '<Plug>Lightspeed_omni_s', {})
 
 -- Resizing panes
--- map("n", "<Left>", ":vertical resize +1<CR>", opts)
--- map("n", "<Right>", ":vertical resize -1<CR>", opts)
--- map("n", "<Up>", ":resize -1<CR>", opts)
--- map("n", "<Down>", ":resize +1<CR>", opts)
+-- map('n', '<Left>', ':vertical resize +1<CR>', opts)
+-- map('n', '<Right>', ':vertical resize -1<CR>', opts)
+-- map('n', '<Up>', ':resize -1<CR>', opts)
+-- map('n', '<Down>', ':resize +1<CR>', opts)
 
 -- Argwrap
-map("n", "<leader>a", "<cmd>ArgWrap<cr>", opts)
+map('n', '<leader>a', '<cmd>ArgWrap<cr>', opts)
