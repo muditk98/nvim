80ed38e3a72c83219746742827f9a3cbd3d61daa
Mudit Kapoor <mukapoor@paypal.com>
2023-06-12 12:16:58 +0530

    Add list chars

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.o.listchars = "tab:▸ ,multispace:·,lead: ,trail:·,nbsp:␣,extends:›,precedes:‹"
+vim.o.list = true
+vim.o.showbreak = "↪"
