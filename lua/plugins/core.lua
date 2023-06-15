50e3dd827a3ab34c586b9195c440c4b0b902e0dd
Mudit Kapoor <mukapoor@paypal.com>
2023-06-15 18:36:17 +0530

    Add a sane surround

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -2,6 +2,16 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   -- "sainnhe/gruvbox-material",
   {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox",
